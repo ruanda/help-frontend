52598d978d4f532ed961bfb5c551663e28b6b0b1
Mateusz Hromada <mhromada@wmi.amu.edu.pl>
2015-02-21 15:00:35 +0100

    Dodane:  * sortowanie zgłoszeń

--- src/js/controller/TicketsController.js
+++ src/js/controller/TicketsController.js
@@ -3,7 +3,19 @@ function($scope, $location, TicketsService) {
 
     $scope.tickets = TicketsService.query();
 
+    $scope.predicate = 'id';
+    $scope.reverse = false;
+
     $scope.show = function(id) {
         $location.path("/tickets/" + id);
+    };
+
+    $scope.order = function(predicate) {
+        if ($scope.predicate === predicate) {
+            $scope.reverse = !$scope.reverse;
+        } else {
+            $scope.predicate = predicate;
+            $scope.reverse = false;
+        }
     };
 }]);
